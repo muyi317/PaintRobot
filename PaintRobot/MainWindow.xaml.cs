cd718eb7fbcd490401a849f47dfbb2f761f7e102
muyi317 <296280063@qq.com>
2021-05-09 20:47:15 +0800

    2021.5.9

--- PaintRobot/MainWindow.xaml.cs
+++ PaintRobot/MainWindow.xaml.cs
@@ -14,6 +14,15 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+
+/* 机械臂控制流程的主程序
+ * 1、通过传送喷涂件型号、尺寸，调用FANUC中对应名称的TP程序
+ * 2、等待FANUC回传两个坐标点，计算变换矩阵
+ * 3、变换矩阵发送给FANUC，FANUC运行修正后的喷涂轨迹
+ * 4、继续等待FANUC回传两个坐标点，计算变换矩阵
+ * 5、下发变换矩阵
+ * 6、等待stop信号*/
+
 namespace PaintRobot
 {
     /// <summary>
@@ -21,41 +30,36 @@ namespace PaintRobot
     /// </summary>
     public partial class MainWindow : Window
     {
-        SocketClient client;
+        private SocketClient m_client;
+        public string m_type;
+        public int m_column;
+        public int m_raw;
+        public int m_length;
+        public int m_width;
 
         public MainWindow()
         {
             InitializeComponent();
             
         }
-
+        // 与FANUC建立通讯
         private void ConnectClick(object sender, RoutedEventArgs e)
         {
-            client = new SocketClient("192.168.1.1", 5233);
-            textConnectState.Text = client.StartClient();
-            Form1_Load();
+            m_client = new SocketClient("192.168.1.1", 5233);
+            textConnectState.Text = m_client.StartClient();
         }
 
+        // 向机械臂发送消息
         private void Btn_send_Click(object sender, RoutedEventArgs e)
         {
-            client.sendData(text_send.Text);
+            m_client.sendData(text_send.Text);
             text_send.Text = "";
         }
-        private void Form1_Load()
-        {
-            
-            //设置定时器          
-            timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(1000);   //时间间隔为一秒
-            timer.Tick += new EventHandler(reciveData);
-            //开启定时器          
-            timer.Start();
-        }
 
+        // 接收机械臂发送的消息
         public void reciveData(object sender, EventArgs e)
         {
-            Thread.Sleep(500);
-            text_recive.Text = client.reciveData();
+            text_recive.Text = m_client.reciveData();
         }
     }
 }
